3d67fd80ac55a8accbdcc53d3b34c41d555fd907
chominsoo <soochomin@gmail.com>
2024-10-28 03:22:26 +0900

    fix/path수정, DTO example추가

--- src/timetable/timetable.controller.ts
+++ src/timetable/timetable.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { TimetableService } from './timetable.service';
 import { CreateDateTimeTableDto } from './dtos/create-date-time-table.dto.ts';
 
-@Controller('timetable')
+@Controller('getTimeSlots')
 export class TimetableController {
   constructor(private readonly timetableService: TimetableService) {}
 
